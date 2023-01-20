29c579b8857732f3a0124f98a23f96f5167d9bb9
carlos-a-g-h <carloselputohabanero@proton.me>
2023-01-20 11:14:39 -0500

    Update and rename ex_YYRecoverSingleAsset.py to ex_YYZRecoverSingleAsset.py

--- examples/ex_YYZRecoverSingleAsset.py
+++ examples/ex_YYZRecoverSingleAsset.py
@@ -3,12 +3,12 @@
 # Example for recovering a single asset from a GMS2 project
 # For this example, provide the path to the asset directory (sprite or sound) as an argument
 
-from personal_gamedev_asset_tools import yy_recover_single
+from personal_gamedev_asset_tools import yyz_recover_single
 from sys import argv
 
 def main():
 	p=argv[1]
-	res=yy_recover_single(".",p)
+	res=yyz_recover_single(".",p)
 	print("\nRecovery results:",res)
 
 main()
