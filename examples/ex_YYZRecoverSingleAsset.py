f3d0f20f9e81776d0a9637f1f0367ee8f15e146e
carlos-a-g-h <carloselputohabanero@proton.me>
2023-02-08 13:51:48 -0500

    Update ex_YYZRecoverSingleAsset.py

--- examples/ex_YYZRecoverSingleAsset.py
+++ examples/ex_YYZRecoverSingleAsset.py
@@ -3,8 +3,9 @@
 # Example for recovering a single asset from a GMS2 project
 # For this example, provide the path to the asset directory (sprite or sound) as an argument
 
-from personal_gamedev_asset_tools import yyz_recover_single
 from sys import argv
+
+from personal_gamedev_asset_tools import yyz_recover_single
 
 def main():
 	p=argv[1]
