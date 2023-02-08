433e71fb2ca086fa089dda307dbf83299dab0af6
carlos-a-g-h <carloselputohabanero@proton.me>
2023-02-08 13:51:09 -0500

    Update ex_YYZRecoverMultipleAssets.py

--- examples/ex_YYZRecoverMultipleAssets.py
+++ examples/ex_YYZRecoverMultipleAssets.py
@@ -3,8 +3,9 @@
 # Example for recovering multiple assets from a GMS2 project
 # For this example, provide paths to the assets separated by a space
 
-from personal_gamedev_asset_tools import yyz_recover_single
 from sys import argv
+
+from personal_gamedev_asset_tools import yyz_recover_single
 
 def main():
 	path_col=argv[1:]
